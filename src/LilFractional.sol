7f0d132fc416c38991959bdc4131966a0f11a747
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-01-06 01:27:37 +0100

    [lil fractional] NatSpec comments

--- src/LilFractional.sol
+++ src/LilFractional.sol
@@ -4,7 +4,16 @@ pragma solidity ^0.8.10;
 import "solmate/tokens/ERC20.sol";
 import "solmate/tokens/ERC721.sol";
 
+/// @title NFT Share Token
+/// @author Miguel Piedrafita
+/// @notice ERC20 token representing a share of an ERC721
 contract NFTShare is ERC20 {
+    /// @notice Deploys an NFTShare with the specified name and symbol, and mints an initial supply to the specified address
+    /// @param name The name of the deployed token
+    /// @param symbol The symbol of the deployed token
+    /// @param mintSupply The supply this token will have, which be minted to the specified address
+    /// @param mintTo The address the initial supply will get minted to
+    /// @dev Deployed from the split() function of the LilFractional contract
     constructor(
         string memory name,
         string memory symbol,
@@ -14,6 +23,10 @@ contract NFTShare is ERC20 {
         _mint(mintTo, mintSupply);
     }
 
+    /// @notice Burns a specified amount of tokens from a specified user after ensuring the caller has permission to
+    /// @param from The address of the user who should get their tokens burned
+    /// @param amount The amount of tokens that should get burned
+    /// @dev The allowance check happens when substracting the amount from the allowed amount. This operation will underflow (and revert) if the caller doesn't have enough allowance.
     function burnFrom(address from, uint256 amount) public payable {
         uint256 allowed = allowance[from][msg.sender]; // Saves gas for limited approvals.
 
@@ -24,9 +37,20 @@ contract NFTShare is ERC20 {
     }
 }
 
+/// @title lil fractional
+/// @author Miguel Piedrafita
+/// @notice Barebones NFT fractionalisation factory.
 contract LilFractional {
+    /// ERRORS ///
+
+    /// @notice Thrown when trying to rejoin a token from a vault that doesn't exist
     error VaultNotFound();
 
+    /// @dev Parameters for vaults
+    /// @param nftContract The ERC721 contract for the fractionalized token
+    /// @param tokenId The ID of the fractionalized token
+    /// @param tokenSupply The amount of issued ERC20 tokens for this vault
+    /// @param tokenContract The ERC20 contract for the issued tokens
     struct Vault {
         ERC721 nftContract;
         uint256 tokenId;
@@ -34,13 +58,32 @@ contract LilFractional {
         NFTShare tokenContract;
     }
 
+    /// EVENTS ///
+
+    /// @notice Emitted when a token is fractionalized
+    /// @param vault The details of the created vault
     event VaultCreated(Vault vault);
+
+    /// @notice Emitted when a token is recovered from a vault
+    /// @param vault The details of the destroyed vault
     event VaultDestroyed(Vault vault);
 
+    /// @notice Used as a counter for the next vault index.
+    /// @dev Initialised at 1 because it makes the first transaction slightly cheaper.
     uint256 internal vaultId = 1;
 
+    /// @notice An indexed list of vaults
+    /// @dev This automatically generates a getter for us!
     mapping(uint256 => Vault) public getVault;
 
+    /// @notice Fractionalize an ERC721 token
+    /// @param nftContract The ERC721 contract for the token you're fractionalizing
+    /// @param tokenId The ID of the token you're fractionalizing
+    /// @param supply The amount of ERC20 tokens to issue for this token. These will be distributed to the caller
+    /// @param name The name for the resultant ERC20 token
+    /// @param symbol The symbol for the resultant ERC20 token
+    /// @return The ID of the created vault
+    /// @dev Remember to call setApprovalForAll(<address of this contract>, true) on the ERC721's contract before calling this function
     function split(
         ERC721 nftContract,
         uint256 tokenId,
@@ -66,6 +109,9 @@ contract LilFractional {
         return vaultId++;
     }
 
+    /// @notice Recover a fractionalized ERC721 token
+    /// @param vaultId The ID of the vault containing the token
+    /// @dev Remember to call approve(<address of this contract>, <supply or greater>) on the ERC20's contract before calling this function
     function join(uint256 vaultId) public payable {
         Vault memory vault = getVault[vaultId];
 
@@ -81,6 +127,7 @@ contract LilFractional {
         );
     }
 
+    /// @dev This function ensures this contract can receive ERC721 tokens
     function onERC721Received(
         address,
         address,
