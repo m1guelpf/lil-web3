f42d48e973e094da1857d29027c2e9bb69b1a618
Miguel Piedrafita <github@miguelpiedrafita.com>
2022-01-20 06:05:29 +0100

    fix wrong EIP name
    closes #9

--- src/LilGnosis.sol
+++ src/LilGnosis.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.10;
 
 /// @title lil gnosis
 /// @author Miguel Piedrafita
-/// @notice An optimised ERC721-based multisig implementation
+/// @notice An optimised ERC712-based multisig implementation
 contract LilGnosis {
 	/// ERRORS ///
 
@@ -45,20 +45,20 @@ contract LilGnosis {
 	/// @notice The amount of required signatures to execute a transaction or change the state
 	uint256 public quorum;
 
-	/// @dev The EIP-721 domain separator
+	/// @dev The EIP-712 domain separator
 	bytes32 public immutable domainSeparator;
 
 	/// @notice A list of signers, and wether they're trusted by this contract
 	/// @dev This automatically generates a getter for us!
 	mapping(address => bool) public isSigner;
 
-	/// @dev EIP-721 types for a signature that updates the quorum
+	/// @dev EIP-712 types for a signature that updates the quorum
 	bytes32 public constant QUORUM_HASH = keccak256('UpdateQuorum(uint256 newQuorum,uint256 nonce)');
 
-	/// @dev EIP-721 types for a signature that updates a signer state
+	/// @dev EIP-712 types for a signature that updates a signer state
 	bytes32 public constant SIGNER_HASH = keccak256('UpdateSigner(address signer,bool shouldTrust,uint256 nonce)');
 
-	/// @dev EIP-721 types for a signature that executes a transaction
+	/// @dev EIP-712 types for a signature that executes a transaction
 	bytes32 public constant EXECUTE_HASH =
 		keccak256('Execute(address target,uint256 value,bytes payload,uint256 nonce)');
 
